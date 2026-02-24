3029a7b41c66548d07e4a2eb4c3ed6fb5a64c39a
V4IDIK <vaidik2001@gmail.com>
2026-02-24 16:16:03 +0530

    Experiment: Increase test set size to 30%

--- src/train.py
+++ src/train.py
@@ -18,7 +18,7 @@ def main():
     # 2. Split data into training and testing sets
     X = df.drop(columns=['target'])
     y = df['target']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     # 3. Train a simple ML model
     print("\nTraining Logistic Regression model...")
